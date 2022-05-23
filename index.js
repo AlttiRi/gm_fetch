ad33f09398a708f21372ef5e51eaf62f9918cc02
[Alt'tiRi] <16310547+AlttiRi@users.noreply.github.com>
2022-05-23 12:10:18 +0300

    Implement `onprogress`

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 // ==UserScript==
-// @name         GM_Fetch Demo
+// @name         GM_fetch Demo (2022.05.22)
 // @namespace    gh.alttiri
-// @description  GM_Fetch (wrapper for GM_xmlhttpRequest) demonstration script
+// @description  GM_fetch (a wrapper for GM_xmlhttpRequest) demonstration script
 // @version      0.1.8-2022.05.22-dev
 // @match        https://example.com/gm_fetch-demo
 // @grant        GM_xmlhttpRequest
+// @connect      ipv4.download.thinkbroadband.com
+// @connect      giant.gfycat.com
 // @connect      example.com
+// @connect      google.com
 // ==/UserScript==
+
+
+let url;
+// url = "http://ipv4.download.thinkbroadband.com/10MB.zip?t=" + Date.now(); // 408 // error
+url = "https://giant.gfycat.com/ShockedSecondaryFiddlercrab.mp4";         // 200   // 32 MB
+// url = "https://example.com/xxx";                                       // 404
+// url = "https://example.com/"; // 200
+// url = "https://google.com/";   // .redirected, .url
 
 // ------------------------------------------------------------------------------------
 // Init
@@ -21,9 +32,13 @@ const fetch = GM_fetch.webContextFetch; // Default `fetch` from web page context
 
 // Just open https://example.com/gm_fetch-demo page to execute this demo
 (async function() {
-    const url = "https://example.com/";
     console.log("GM_fetch:", url);
-    const response = await GM_fetch(url);
+    const response = await GM_fetch(url, {
+        extra: {
+            useStream: true,
+            onprogress: ({loaded, total, lengthComputable}) => {console.log({loaded, total, lengthComputable});}
+        }
+    });
     console.log(response);
 
     const {status, statusText} = response;
@@ -68,15 +83,15 @@ function getGM_fetch() {
     }
 
     /** The default Response always has {type: "default", redirected: false, url: ""} */
-    class ExResponse extends Response {
-        [Symbol.toStringTag] = "ExResponse";
-        constructor(body, {headers, status, statusText, url, finalUrl}) {
+    class ResponseEx extends Response {
+        [Symbol.toStringTag] = "ResponseEx";
+        constructor(body, {headers, status, statusText, url, redirected}) {
             super(body, {status, statusText, headers: {
                     ...headers,
-                    "content-type": headers.get("content-type").split("; ")[0] // Fixes Blob type ("text/html; charset=UTF-8")
+                    "content-type": headers.get("content-type").split("; ")[0] // Fixes Blob type ("text/html; charset=UTF-8") in TM
                 }});
-            this._url = finalUrl;
-            this._redirected = url !== finalUrl;
+            this._url = url;
+            this._redirected = redirected;
             this._headers = headers; // `HeadersLike` is more user-friendly for debug than the original `Headers` object
         }
         get redirected() { return this._redirected; }
@@ -85,17 +100,21 @@ function getGM_fetch() {
         /** @returns {HeadersLike} */
         get headers() { return this._headers; }
     }
-    class HeadersLike {
+    class HeadersLike { // Note: the original `Headers` throws an error if `key` requires `.trim()`
         constructor(headers) {
             headers?.entries().forEach((key, value) => {
                 this.append(key, value);
             });
         }
         get(key) {
-            return this[key.toLowerCase()];
+            const value = this[key.trim().toLowerCase()];
+            return value === undefined ? null : value;
         }
         append(key, value) {
             this[key.trim().toLowerCase()] = value.trim();
+        }
+        has(key) {
+            return this.get(key) !== null;
         }
     }
     /**
@@ -130,6 +149,46 @@ function getGM_fetch() {
         json() {        return this._blobPromise.then(blob => blob.text()).then(text => JSON.parse(text)); }
     }
 
+    const identityContentEncodings = new Set([null, "identity", "no encoding"]);
+    function responseProgressProxy(response, onProgress) {
+        const lengthComputable = identityContentEncodings.has(response.headers.get("Content-Encoding"));
+        const compressed = !lengthComputable;
+        const contentLength = parseInt(response.headers.get("Content-Length"));
+        const total = lengthComputable ? (isNaN(contentLength) ? 0 : contentLength) : 0; // Original XHR behaviour; in TM it equals to `contentLength`.
+
+        let loaded = 0;
+        const reader = response.body.getReader();
+        const readableStream = new ReadableStream({
+            async start(controller) {
+                while (true) {
+                    const {done, /** @type {Uint8Array} */ value} = await reader.read();
+                    if (done) {
+                        break;
+                    }
+                    loaded += value.length;
+                    try {
+                        onProgress({
+                            loaded, total, lengthComputable,
+                            contentLength, compressed
+                        });
+                    } catch (e) {
+                        console.error("[onProgress]:", e);
+                    }
+                    controller.enqueue(value);
+                }
+                controller.close();
+                reader.releaseLock();
+            },
+            cancel() {
+                void reader.cancel();
+            }
+        });
+        return new ResponseEx(readableStream, response);
+    }
+
+
+    const HEADERS_RECEIVED = 2;
+
     /**
      * The simplified `fetch` — a wrapper for `GM_xmlHttpRequest`.
      * @example
@@ -140,26 +199,30 @@ function getGM_fetch() {
      const blob = await response.blob();
      * @return {Promise<Response>} */
     async function GM_fetch(url, fetchInit = {}) {
-        const defaultFetchInit = {
-            method: "get",
-            // Extra props:
-            useStream: true, webContext: false, GM_extraOpts: {}
+        const defaultFetchInit = {method: "get"};
+        const defaultExtra = {useStream: true, webContext: false, onprogress: null};
+        const opts = {
+            ...defaultFetchInit,
+            ...fetchInit,
+            extra: {
+                ...defaultExtra,
+                ...fetchInit.extra
+            }
         };
-        const opts = {...defaultFetchInit, ...fetchInit};
-        if (opts.webContext) {
-            delete opts.useStream;
-            delete opts.webContext;
-            delete opts.GM_extraOpts;
+        if (opts.extra.webContext) {
+            delete opts.extra;
             return fetch(url, opts);
         }
 
-        const {headers, method, useStream, GM_extraOpts} = opts;
-        const HEADERS_RECEIVED = 2;
+        const {headers, method, extra: {useStream, onprogress}} = opts;
+        delete opts.extra.webContext;
+        delete opts.extra.useStream;
+
         if (!isStreamSupported || !useStream) {
             return new Promise((resolve, _reject) => {
                 const blobPromise = new Promise((resolve, reject) => {
                     GM_xmlhttpRequest({
-                        ...GM_extraOpts,
+                        ...opts.extra,
                         url,
                         method,
                         headers,
@@ -167,6 +230,7 @@ function getGM_fetch() {
                         onload: (response) => resolve(response.response),
                         onerror: reject,
                         onreadystatechange: onHeadersReceived,
+                        onprogress
                     });
                 });
                 blobPromise.catch(_reject);
@@ -187,12 +251,11 @@ function getGM_fetch() {
             return new Promise((resolve, _reject) => {
                 const responsePromise = new Promise((resolve, reject) => {
                     void GM_xmlhttpRequest({
-                        ...GM_extraOpts,
+                        ...opts.extra,
                         url,
                         method,
                         headers,
-                        responseType: "stream",
-                        fetch: true,
+                        responseType: "stream", /* fetch: true, */
                         onerror: reject,
                         onreadystatechange: onHeadersReceived
                     });
@@ -204,7 +267,11 @@ function getGM_fetch() {
                     } = gmResponse;
                     if (readyState === HEADERS_RECEIVED) {
                         const headers = parseHeaders(responseHeaders);
-                        const newResp = new ExResponse(readableStream, {headers, status, statusText, url, finalUrl});
+                        const redirected = url !== finalUrl;
+                        let newResp = new ResponseEx(readableStream, {headers, status, statusText, url, redirected});
+                        if (onprogress) {
+                            newResp = responseProgressProxy(newResp, onprogress);
+                        }
                         resolve(newResp);
                     }
                 }
